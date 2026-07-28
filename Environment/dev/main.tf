651d029ffd2768d21c0f4e8d52df34d578c3c4f4
Vijay <vijay@example.com>
2026-07-28 10:48:20 +0530

    code release

--- Environment/dev/main.tf
+++ Environment/dev/main.tf
@@ -1,36 +1,36 @@
 
 module "azurerm_resource_group" {
-  source = "../module/azurerm_resource_group"
+  source = "../../module/azurerm_resource_group"
   rgs    = var.rgs
 }
 
 module "azurerm_storage_account" {
   depends_on       = [module.azurerm_resource_group]
-  source           = "../module/azurerm_storage_account"
+  source           = "../../module/azurerm_storage_account"
   storage_accounts = var.storage_accounts
 }
 
 module "azurerm_virtual_network" {
   depends_on = [module.azurerm_resource_group]
-  source     = "../module/azurerm_virtual_network"
+  source     = "../../module/azurerm_virtual_network"
   vnets      = var.vnets
 }
 
 module "azurerm_subnet" {
   depends_on = [module.azurerm_virtual_network, module.azurerm_resource_group]
-  source     = "../module/azurerm_subnet"
+  source     = "../../module/azurerm_subnet"
   subnets    = var.subnets
 }
 
 module "azurerm_public_IP" {
   depends_on = [module.azurerm_resource_group]
-  source     = "../module/azurerm_public_IP"
+  source     = "../../module/azurerm_public_IP"
   public_ips = var.public_ips
 }
 
 module "azurerm_key_vault" {
   depends_on = [module.azurerm_resource_group]
-  source     = "../module/azurerm_key_vault"
+  source     = "../../module/azurerm_key_vault"
   key_vaults = var.key_vaults
 }
 
@@ -46,6 +46,6 @@ locals {
 
 module "azurerm_virtual_machine" {
   depends_on = [module.azurerm_subnet, module.azurerm_public_IP, module.azurerm_resource_group, module.azurerm_key_vault]
-  source     = "../module/azurerm_virtual_machine"
+  source     = "../../module/azurerm_virtual_machine"
   vms        = local.vms_with_password
 }
